2c34a719aacbd1ebc219e7e974c6623e6a097c6c
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-10-26 16:44:02 +0530

    Added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react'
 import { Route, Routes, useLocation } from "react-router-dom";
+import initFontAwesome from "./components/FontAwesomeIcons/initFontAwesome";
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import About from './pages/About';
+
+initFontAwesome();
 
 function App() {  
 
